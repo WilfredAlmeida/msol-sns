733a0f28dc1f080205cf3e389022ae1e765267ab
Wilfred Almeida <almeidawilfred642@gmail.com>
2023-08-12 22:49:06 +0530

    feat: Homepage UI completed
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,69 @@
+<script lang="ts">
+	export let showSnsCreated = false;
+	export let snsString = '';
+	export let hasErrroed = false;
+	export let errorString = '';
+	export let isLoading = false;
+
+	const formSubmit = async (event: { target: HTMLFormElement | undefined }) => {
+		isLoading = true;
+
+		const formData = new FormData(event.target);
+		const sns = formData.get('sns');
+		const publicKey = formData.get('public-key');
+
+		if (!sns || !/^[A-Za-z0-9\-]{3,20}$/.test(`${sns}`))
+			return alert(`SNS must 3 to 20 characters and only contain letters, numbers, and hyphens`);
+
+		if (!publicKey) return alert(`Invalid Public Key`);
+
+		const res = await fetch('/api/sns', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				sns,
+				publicKey
+			})
+		});
+
+		const responseJson = await res.json();
+
+		if (responseJson.status === 'MSOL_SNS_CREATED') {
+			showSnsCreated = true;
+			snsString = `/${sns}`;
+			isLoading = false;
+			return;
+		}
+
+		switch (responseJson.status) {
+			case 'MSOL_SNS_EXISTS':
+				errorString = 'SNS already exists';
+				break;
+			case 'MSOL_INPUT_INVALID':
+				errorString = 'Invalid SNS or Public Key';
+				break;
+			case 'MSOL_INVALID_SNS':
+				errorString = 'Invalid SNS';
+				break;
+			case 'MSOL_SOMETHING_WENT_WRONG':
+				errorString = 'Something went wrong';
+				break;
+			default:
+				errorString = 'Unknown Error';
+				break;
+		}
+		hasErrroed = true;
+        showSnsCreated = false;
+		isLoading = false;
+	};
+
+	const copyToClipboard = () => {
+		navigator.clipboard.writeText(snsString);
+	};
+</script>
+
 <nav class="bg-gradient-to-br from-indigo-500 to-black">
 	<div
 		class="
@@ -38,21 +104,21 @@
 			>
 				<li>
 					<a
-						href="#"
+						href="/"
 						class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
 						>Home</a
 					>
 				</li>
 				<li>
 					<a
-						href="#"
+						href=""
 						class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
 						>Docs</a
 					>
 				</li>
 				<li>
 					<a
-						href="#"
+						href="/"
 						class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
 						>About</a
 					>
@@ -64,15 +130,16 @@
 
 <div class="mx-auto max-w-500 mt-20 w-full flex justify-center items-center">
 	<div class="bg-gray-800 glassmorphic-6 shadow-lg rounded-lg p-8">
-		<form>
+		<form on:submit|preventDefault={formSubmit}>
 			<div class="mb-6">
 				<label class="text-white text-lg font-bold" for="sns">SNS</label>
 				<input
 					class="bg-gray-700 text-white border-gray-600 rounded-lg p-4 w-full"
 					id="sns"
+					name="sns"
 					type="text"
 					placeholder="Enter your SNS"
-                    required
+					required
 				/>
 			</div>
 			<div class="mb-6">
@@ -80,39 +147,70 @@
 				<input
 					class="bg-gray-700 text-white border-gray-600 rounded-lg p-4 w-full"
 					id="public-key"
+					name="public-key"
 					type="text"
 					placeholder="Enter your Public Key"
-                    required
+					required
 				/>
 			</div>
-			<button
-				class="
+
+			{#if !isLoading}
+            <div class="flex justify-center items-center">
+				<button
+					class="
         bg-gradient-to-bl from-purple-600 to-blue-300 text-white font-bold py-2 px-4 rounded-lg transition-transform transform hover:scale-105 active:scale-95
         "
-				type="submit">Submit</button
-			>
+					type="submit">Submit</button
+				>
+                </div>
+			{:else}
+            <div class="flex justify-center items-center">
+                <div class="w-16 h-16 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
+              </div>
+              
+              
+			{/if}
 		</form>
 	</div>
 </div>
 
-<div class="mx-auto max-w-500 mt-20 w-full flex justify-center items-center">
-	<div class="bg-gray-800 glassmorphic-6 shadow-lg rounded-lg p-8">
-		<div class="flex justify-center">
-			<h3 class="text-white text-3xl font-bold">SNS Created Successfully</h3>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				class="h-6 w-6"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke="white"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 012-2h2a2 2 0 012 2v2M9 5a2 2 0 012-2h2a2 2 0 012 2v2M5 9h14M5 13h14M5 17h7"
-				/>
-			</svg>
+{#if showSnsCreated}
+	<div class="mx-auto max-w-500 mt-20 w-full flex justify-center items-center">
+		<div class="bg-gray-800 glassmorphic-6 shadow-lg rounded-lg p-8">
+			<div class="flex justify-center">
+				<h3 class="text-white text-3xl font-bold">SNS Created Successfully</h3>
+			</div>
+			<div class="flex justify-center mt-5">
+				<a href={snsString} class="text-green-400 text-2xl font-bold"
+					>{`${window.location.origin.split('//')[1] + snsString}`}</a
+				>
+				<button on:click|preventDefault={copyToClipboard} data-te-toggle="tooltip" title="Copy">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-6 w-6 cursor-pointer"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="white"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 012-2h2a2 2 0 012 2v2M9 5a2 2 0 012-2h2a2 2 0 012 2v2M5 9h14M5 13h14M5 17h7"
+						/>
+					</svg>
+				</button>
+			</div>
 		</div>
 	</div>
-</div>
+{/if}
+
+{#if hasErrroed}
+	<div class="mx-auto max-w-500 mt-20 w-full flex justify-center items-center">
+		<div class="bg-gray-800 glassmorphic-6 shadow-lg rounded-lg p-8">
+			<div class="flex justify-center">
+				<h3 class="text-red-400 text-3xl font-bold">{errorString}</h3>
+			</div>
+		</div>
+	</div>
+{/if}
