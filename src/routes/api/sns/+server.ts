22bfaac2a4b32b88083807d41a5929ca3d2ce0d9
Wilfred Almeida <almeidawilfred642@gmail.com>
2023-08-12 12:26:12 +0530

    feat: constants, standardized API response
    Signed-off-by: Wilfred Almeida <almeidawilfred642@gmail.com>

--- src/routes/api/sns/+server.ts
+++ src/routes/api/sns/+server.ts
@@ -1,91 +1,118 @@
 import { json } from "@sveltejs/kit";
 import { kv, createClient } from "@vercel/kv";
-// import { $env } from '@sveltejs/kit';
 
-import {KV_REST_API_URL, KV_REST_API_TOKEN} from "$env/static/private"
+import { KV_REST_API_URL, KV_REST_API_TOKEN } from "$env/static/private"
+import { HttpResponseCode, MsolStatusCodes } from "../utils/constants.js";
 const snsDb = createClient({
     url: KV_REST_API_URL,
     token: KV_REST_API_TOKEN,
 });
 
-export async function POST({request}){
+export async function POST({ request }) {
     const body = await request.json()
-    const {publicKey, sns} = body
-    if(!publicKey || !sns){
+    const { publicKey, sns } = body
+    if (!publicKey || !sns) {
         return json(
-			{ message: 'Invalid sns or publicKey' },
-			{
-				status: 400
-			}
-		);
+            {
+                status: MsolStatusCodes.MSOL_INPUT_INVALID,
+                data: null,
+                error: [{ message: 'Invalid sns or publicKey' }]
+            },
+            {
+                status: HttpResponseCode.BAD_REQUEST
+            }
+        );
     }
 
-    if((`${sns}`).trim().length > 20){
+    if ((`${sns}`).trim().length > 20) {
         return json(
-			{ message: 'Invalid sns. sns cannot be more than 20 characters.' },
-			{
-				status: 400
-			}
-		);
+            {
+                status: MsolStatusCodes.MSOL_INPUT_INVALID,
+                data: null,
+                error: [{ message: 'Invalid sns. sns cannot be more than 20 characters.' }]
+            },
+            {
+                status: HttpResponseCode.BAD_REQUEST
+            }
+        );
     }
 
     // TODO: add public key validation
 
-    const res = await snsDb.set(`${sns}`,`${publicKey}`)
+    const res = await snsDb.set(`${sns}`, `${publicKey}`)
 
-    if(!res){
+    if (!res) {
         return json(
-            {message: "Something went wrong. Please try again"},
             {
-                status: 500
+                status: MsolStatusCodes.MSOL_SOMETHING_WENT_WRONG,
+                data: null,
+                error: [{ message: 'Something went wrong. Please try again' }],
+            },
+            {
+                status: HttpResponseCode.INTERNAL_SERVER_ERROR
             }
         )
     }
 
     console.log(res);
-    
+
     return json(
-        {message: "SNS successfully created."},
         {
-            status: 201
+            status: MsolStatusCodes.MSOL_SNS_CREATED,
+            data: null,
+            error: null
+        },
+        {
+            status: HttpResponseCode.CREATED
         }
     )
 
 }
 
-export async function GET({params, url}){
-    console.log("PARAMS");
-    console.log(params);
-    
+export async function GET({ url }) {
+
     const sns = url.searchParams.get("sns");
     console.log(sns);
-    if(!sns){
+
+    if (!sns) {
         return json(
-			{ message: 'Invalid sns' },
-			{
-				status: 400
-			}
-		);
+            {
+                status: MsolStatusCodes.MSOL_INPUT_INVALID,
+                data: null,
+                error: [{ message: 'Invalid sns' }]
+            },
+            {
+                status: HttpResponseCode.BAD_REQUEST
+            }
+        );
     }
 
     const res = await snsDb.get(sns)
     console.log(res);
-    
 
-    if(!res){
+
+    if (!res) {
         return json(
-			{ message: 'sns not found' },
-			{
-				status: 404
-			}
-		);
+            {
+                status: MsolStatusCodes.MSOL_SNS_NOT_FOUND,
+                data: null,
+                error: null
+            },
+            {
+                status: HttpResponseCode.NOT_FOUND
+            }
+        );
     }
 
     return json(
-        { message: 'sns found', data:[{publicKey: res, sns}] },
         {
-            status: 404
+            status: MsolStatusCodes.MSOL_INPUT_INVALID,
+            data: [{ publicKey: res, sns }],
+            error: null
+        },
+        {
+            status: HttpResponseCode.SUCCESS
         }
     );
-    
+
 }
